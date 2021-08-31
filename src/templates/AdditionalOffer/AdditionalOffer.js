474672e353da8b332e5592e4dcb947967bf9d1e3
DawidWebb <tomasz.matras@developerweb.pl>
2021-09-01 01:14:54 +0200

    add @medya Querries for: AdditionalOffer, Application, Support, Fotter.

--- src/templates/AdditionalOffer/AdditionalOffer.js
+++ src/templates/AdditionalOffer/AdditionalOffer.js
@@ -3,24 +3,26 @@ import styles from "./additionalOffer.module.scss";
 const AdditionalOffer = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>
-        <h2>pakiet 20 gb lte</h2>
-        <p>promocja</p>
-      </div>
-      <div className={styles.offer}>
-        <p>Dla tych którzy nie wyobrażają sobie życia bez internetu:</p>
-        <ul>
-          <li>ogladaj filmy umieszczone w sieci</li>
-          <li>ściągaj duże pliki</li>
-          <li>korzystaj z poczty, komunikatora</li>
-          <li>surfuj swobodnie</li>
-        </ul>
-      </div>
-      <div className={styles.info}>
-        <p>swoboda</p>
-        <p>i mobilność za</p>
-        <p>50</p>
-        <p>zł</p>
+      <div className={styles.inside}>
+        <div className={styles.title}>
+          <h2>pakiet 20 gb lte</h2>
+          <p>promocja</p>
+        </div>
+        <div className={styles.offer}>
+          <p>Dla tych którzy nie wyobrażają sobie życia bez internetu:</p>
+          <ul>
+            <li>ogladaj filmy umieszczone w sieci</li>
+            <li>ściągaj duże pliki</li>
+            <li>korzystaj z poczty, komunikatora</li>
+            <li>surfuj swobodnie</li>
+          </ul>
+        </div>
+        <div className={styles.info}>
+          <p>swoboda</p>
+          <p>i mobilność za</p>
+          <p>50</p>
+          <p>zł</p>
+        </div>
       </div>
     </div>
   );
